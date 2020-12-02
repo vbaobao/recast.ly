0af566fec019444a6403ff5e14c9e24c0e32aba6
vbaobao <vbao.main@gmail.com>
2020-12-01 19:51:48 -0800

    Complete conversion of App.jsx to ES6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,35 @@ import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><Search /></div>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoList: exampleVideoData, //videoID as identifier
+      currentVideo: exampleVideoData[0]
+    };
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><Search /></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><VideoPlayer video={this.props.currentVideo} /></div>
+          </div>
+          <div className="col-md-5">
+            <div><VideoList videos={this.props.videoList} /></div>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><VideoPlayer video={exampleVideoData[0]} /></div>
-      </div>
-      <div className="col-md-5">
-        <div><VideoList videos={exampleVideoData} /></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
