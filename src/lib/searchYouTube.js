c7691436f1b879ec27c652b950a33535fca59622
vbaobao <vbao.main@gmail.com>
2020-12-03 19:11:09 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,26 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: `https://youtube.googleapis.com/youtube/v3/search`,
+    type: 'GET',
+    data: {
+      type: 'video',
+      videoEmbeddable: true,
+      part: 'snippet',
+      maxResults: options.max || 5,
+      q: options.query || 'react',
+      key: options.key
+    },
+    contentType: 'application/json',
+    success: function(response) {
+      console.log(response);
+      callback(response.items);
+    },
+    error: function(error) {
+      console.error('YoutubeAPI: Failed to fetch', error);
+    }
+  });
 };
 
 export default searchYouTube;
+
+// https://www.googleapis.com/youtube/v3/search
